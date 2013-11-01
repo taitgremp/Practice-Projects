93415fb98b26b97e4e758b6dd76570da5782c357
U-RECONDOTECH\gabriel.tait <gabriel.tait@RTGTait01.RecondoTech.com>
2013-11-01 16:19:31 -0600

    Some Message

---
+++ Magic/src/card/types/Instant.java
@@ -0,0 +1,11 @@
+package card.types;
+
+import java.util.HashMap;
+
+public class Instant extends BaseCard {
+	
+	public Instant(HashMap<String, Integer> costObj, String instantText){
+		super(costObj, instantText);
+	}
+
+}
